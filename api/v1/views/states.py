7307dbdf98b6d59dcdf150964fa53550820bd7ca
aasemgar7i <aasemgar7i@gamil.com>
2024-04-27 08:51:07 +0300

    hf

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import abort
-import json
 from api.v1.views import app_views
 from models import storage
 from models.state import State

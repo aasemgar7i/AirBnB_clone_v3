8c50fdce9fc5d14976fe7d60b800cb249d9de4e7
aasemgar7i <aasemgar7i@gamil.com>
2024-04-27 07:01:12 +0300

    404

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,21 @@
 """AirBNB v3 Api"""
 import os
 from flask import Flask
+from flask import jsonify
+from flask import make_response
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.errorhandler(404)
+def not_found(error):
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
 
 
 @app.teardown_appcontext
